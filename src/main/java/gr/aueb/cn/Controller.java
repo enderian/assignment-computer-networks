6c6c49d3a556623c6d5fded9043229171b0037dd
MrGoumX <MrGoumX@users.noreply.github.com>
2019-04-10 18:28:14 +0300

    Added all requests

--- src/main/java/gr/aueb/cn/Controller.java
+++ src/main/java/gr/aueb/cn/Controller.java
@@ -4,13 +4,13 @@ public class Controller {
     public static void main(String[] args) {
         Distributor server = new Distributor(4200);
 
-        EnergyUser user1 = new EnergyUser(100);
+        /*EnergyUser user1 = new EnergyUser(100);
         EnergyUser user2 = new EnergyUser(50);
 
         user1.issueConnection("localhost", 4200);
         user1.dump();
         user1.setAvailable_energy(20);
         user2.issueConnection("localhost", 4200);
-
+*/
     }
 }
--- src/main/java/gr/aueb/cn/EnergyUser.java
+++ src/main/java/gr/aueb/cn/EnergyUser.java
@@ -4,6 +4,7 @@ import org.omg.PortableServer.THREAD_POLICY_ID;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class EnergyUser {
 
@@ -22,17 +23,69 @@ public class EnergyUser {
     public void menu() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String option = "";
+        System.out.println("Menu:");
+        System.out.println("0. Sign in");
+        System.out.println("1. Request energy");
+        System.out.println("2. Request energy from specific user");
         while((option = reader.readLine()) != null){
             if(option.equalsIgnoreCase("0")){
+                System.out.println("Give IP of server");
                 ip = reader.readLine();
+                System.out.println("Give port of server");
                 port = Integer.parseInt(reader.readLine());
                 issueConnection(ip, port);
             }
             else if(option.equalsIgnoreCase("1")){
-
+                System.out.println("1. Request with second backup");
+                System.out.println("2. Request with delay");
+                String opt2 = reader.readLine();
+                if(opt2.equalsIgnoreCase("1")){
+                    System.out.println("How much energy do you need?");
+                    int en = Integer.parseInt(reader.readLine());
+                    EnergyRequest er = new EnergyRequest(en);
+                    sendEnergyRequest(er);
+                }
+                else if(opt2.equalsIgnoreCase("2")){
+                    System.out.println("How much energy do you need?");
+                    int en = Integer.parseInt(reader.readLine());
+                    System.out.println("How much delay do you want before trying again?");
+                    long time = Long.parseLong(reader.readLine());
+                    EnergyRequest er = new EnergyRequestWait(en, time);
+                    sendEnergyRequest(er);
+                }
+            }
+            else if(option.equalsIgnoreCase("3")){
+                try {
+                    ArrayList<EnergyUser> currentUsers = (ArrayList<EnergyUser>)in.readObject();
+                    for(EnergyUser eu : currentUsers){
+                        System.out.println("User with ip: " + eu.ip);
+                    }
+                    System.out.println("Give the IP of the user you want to receive energy");
+                    String req_ip = reader.readLine();
+                    EnergyUser found = null;
+                    for(EnergyUser eu : currentUsers){
+                        if(eu.ip.equalsIgnoreCase(req_ip)){
+                            found = eu;
+                        }
+                    }
+                    if(found != null){
+                        System.out.println("Give energy required");
+                        int en = Integer.parseInt(reader.readLine());
+                        EnergyRequestToEU req = new EnergyRequestToEU(en, found);
+                        sendEnergyRequest(req);
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                System.out.println("Wrong option! Select again!");
+                System.out.println("Menu:");
+                System.out.println("0. Sign in");
+                System.out.println("1. Request energy");
+                System.out.println("2. Request energy from specific user");
             }
         }
-
     }
 
     public void issueConnection(String ip, int port){
@@ -57,6 +110,15 @@ public class EnergyUser {
             };
             new Thread(runnable).start();
             //out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendEnergyRequest(EnergyRequest er){
+        try {
+            out.writeObject(er);
+            out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
